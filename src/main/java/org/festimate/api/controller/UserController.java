43754dd10d8023dbec24b0b24e19b254d7cc5bf2
2hyunjinn <huenjin04@naver.com>
2024-11-20 16:31:29 +0900

    [feat]#5 유저 정보 조회 API

--- src/main/java/org/festimate/api/controller/UserController.java
+++ src/main/java/org/festimate/api/controller/UserController.java
@@ -2,13 +2,11 @@ package org.festimate.api.controller;
 
 import jakarta.validation.Valid;
 import org.festimate.api.dto.request.SignUpRequest;
+import org.festimate.api.dto.response.UserDetailResponse;
 import org.festimate.api.dto.response.UserResponse;
 import org.festimate.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/festimate/v1")
@@ -23,7 +21,15 @@ public class UserController {
     public ResponseEntity<UserResponse> signup(
             @Valid @RequestBody SignUpRequest signupRequest
     ) {
-        Long memberId = userService.signupUser(signupRequest.getUsername(), signupRequest.getNickname(), signupRequest.getAge(), signupRequest.getGender(), signupRequest.getSchool(), signupRequest.getHeight(), signupRequest.getMbti(), signupRequest.getAppearanceList());
-        return ResponseEntity.ok(new UserResponse(memberId));
+        Long userId = userService.signupUser(signupRequest.getUsername(), signupRequest.getNickname(), signupRequest.getAge(), signupRequest.getGender(), signupRequest.getSchool(), signupRequest.getHeight(), signupRequest.getMbti(), signupRequest.getAppearanceList());
+        return ResponseEntity.ok(new UserResponse(userId));
+    }
+
+    @GetMapping("/user-detail")
+    public ResponseEntity<UserDetailResponse> getUserDetails(
+            @Valid @RequestHeader("userId") Long userId
+    ) {
+        UserDetailResponse userDetail = userService.getUserDetailById(userId);
+        return ResponseEntity.ok(userDetail);
     }
 }
