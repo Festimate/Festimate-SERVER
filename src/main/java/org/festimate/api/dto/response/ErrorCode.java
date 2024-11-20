d2806b9b5057906a91e4634aac23f9ca0fb8fef9
2hyunjinn <huenjin04@naver.com>
2024-11-20 16:23:17 +0900

    [chore]#5 ErrorCode 추가

--- src/main/java/org/festimate/api/dto/response/ErrorCode.java
+++ src/main/java/org/festimate/api/dto/response/ErrorCode.java
@@ -4,6 +4,7 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "e4000", "유효하지 않은 요청입니다."),
     INVALID_INPUT_HTTP(405, "e4050", "잘못된 HTTP method 요청입니다."),
     INVALID_INPUT_USER_DUPLICATE(409, "e4090", "이미 존재하는 회원입니다."),
+    USER_NOT_FOUND(409, "e4091", "존재하지 않은 회원입니다."),
     INTERNAL_SERVER_ERROR(500, "e5000", "서버 내부 오류입니다.");
 
     private final int code;
